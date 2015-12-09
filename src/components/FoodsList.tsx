34c98e5706953b38904c5ad6cb2610a26eea8c2f
Pawel Niewiadomski <11110000b@gmail.com>
2015-12-09 07:57:39 +0100

    using bootstrap cols

--- src/components/FoodsList.tsx
+++ src/components/FoodsList.tsx
@@ -26,13 +26,27 @@ class FoodsList extends React.Component<Props, any> {
       return whatFood === '' || food.question.toLowerCase().indexOf(whatFood.toLowerCase()) > 0;
     }).map((food) => {
       return (
-        <Food food={food}/>
+          <Food food={food}/>
         );
     });
 
+    var rows = [], accu = [];
+    for (var i = 0; i < list.length; ++i) {
+      accu.push(
+        (<div className="col-md-6 col-lg-3">{ list[i]}</div>)
+      );
+      
+      if ((i + 1) % 4 == 0) {
+        rows.push(
+          (<div className="row">{accu}</div>)
+        );
+        accu = [];
+      }
+    };
+
     return (
       <div>
-        {list}
+        {rows}
       </div>
     );
   }
