853a64b65f5ed871fae10dae27d7c76644befcf4
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-09 13:13:20 +0200

    removed header

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -5,15 +5,14 @@ import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
-    <Header />
     <div className={classes.mainContainer}>
       {children}
     </div>
   </div>
-)
+);
 
 CoreLayout.propTypes = {
   children: React.PropTypes.element.isRequired
-}
+};
 
 export default CoreLayout
